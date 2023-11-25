9832c1eeb6b5395bbd45723a015a0cb4d55a9fa6
manishdoley23 <manishdoley23@gmail.com>
2023-11-25 19:01:28 +0530

    bug fix

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -3,8 +3,6 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 const client = new S3Client({
 	region: process.env.AWS_REGION,
 	credentials: {
-		// accessKeyId: "AKIA3AP5PFREROBWPDG7",
-		// secretAccessKey: "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",
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
 	},
